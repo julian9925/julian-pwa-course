cc2b14d7315cc8cc8f10057c84766e3a63698af2
Julian Chen <julian9925@gmail.com>
2022-09-04 17:35:02 +0800

    feat: add update popup and cache network first strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {SwPush, SwUpdate} from "@angular/service-worker";
+import {SwPush, SwUpdate} from '@angular/service-worker';
 
 
 @Component({
@@ -10,15 +10,20 @@ import {SwPush, SwUpdate} from "@angular/service-worker";
 export class AppComponent  implements OnInit {
 
 
-    constructor() {
+    constructor(
+      private swUpdate: SwUpdate
+    ) {
 
     }
 
     ngOnInit() {
-
-
-
+      if (this.swUpdate.isEnabled) {
+        this.swUpdate.available.subscribe(() => {
+          console.log('New Update');
+          if (confirm('New version available. Load New Version?')) {
+            window.location.reload();
+          }
+        });
+      }
     }
-
 }
-
