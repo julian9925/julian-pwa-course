173d16781b8b3c8d0850cd54b4392b8c928094f4
Julian Chen <julian9925@gmail.com>
2022-09-04 10:41:18 +0800

    feat: add service worker intital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,14 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {LessonsComponent} from './lessons/lessons.component';
 
-import {LessonsService} from "./services/lessons.service";
-import {ReactiveFormsModule} from "@angular/forms";
+import {LessonsService} from './services/lessons.service';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {environment} from '../environments/environment.prod';
 import {ServiceWorkerModule} from '@angular/service-worker';
 
-
-
-
-
-
-
-
-
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AppRoutingModule} from "./app-routing.module";
-
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
     declarations: [
@@ -35,7 +24,8 @@ import {AppRoutingModule} from "./app-routing.module";
         HttpClientModule,
         BrowserAnimationsModule,
         AppRoutingModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
     ],
     providers: [
         LessonsService
